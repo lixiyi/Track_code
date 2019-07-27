84358ec6f5739522b869d726c7ce1e2b4a521ed5
lixiyi <609991496@qq.com>
2019-07-27 14:14:31 +0000

    add bert, model path

--- src/DataProcess/getCfg.py
+++ src/DataProcess/getCfg.py
@@ -1,5 +1,7 @@
 DOCUMENT_COUNT = 595037 - 23074
 OUTPUT = '/home/trec7/lianxiaoying/Track_code/src/outputs/'
+BERT_PATH = '/home/trec7/lianxiaoying/bert/'
+BERT_MODEL = '/home/trec7/lianxiaoying/model/uncased_L-12_H-768_A-12/'
 
 # get file path conf
 def get_path_conf(filename):
