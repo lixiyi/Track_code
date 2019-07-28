fd2f37819c0df5b5f17b7a58ceb3b3db4840264c
lixiyi <609991496@qq.com>
2019-07-28 22:57:21 +0800

    checkdata

--- src/DataProcess/CheckData.py
+++ src/DataProcess/CheckData.py
@@ -8,14 +8,15 @@ import numpy as np
 path_mp = cfg.get_path_conf('../path.cfg')
 
 with open(path_mp['DataPath'] + path_mp['WashingtonPost'], 'r', encoding='utf-8') as f:
-	cnt = 1
+	cnt = 0
 	for line in tqdm(f):
 		obj = json.loads(line)
 		title = obj['title']
 		author = obj['author']
 		date = obj['published_date']
+		cnt += 1
 		try:
 			data = int(data)
 		except:
-			print(cnt)
+			print(cnt, data)
 
