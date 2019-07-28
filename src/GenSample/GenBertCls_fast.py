986ccda66d63459fd9781cb22b35429787cac379
lixiyi <609991496@qq.com>
2019-07-29 00:40:06 +0800

    modify

--- src/GenSample/GenBertCls_fast.py
+++ src/GenSample/GenBertCls_fast.py
@@ -79,106 +79,107 @@ def gen_sample(args=None):
 	print('TF-IDF idx loaded.')
 
 	filter_kicker = {"Opinion": 1, "Letters to the Editor": 1, "The Post's View": 1}
-	for cnt in range(1, len(WashingtonPost)):
-		kicker_filterd_mp = {}		# Record line filtered by kicker, line index start from 1
-		line = WashingtonPost[cnt]
-		obj = json.loads(line)
-		contents = obj['contents']
-		title = obj['title']
-		author = obj['author']
-		date = obj['published_date']
-		skip = False
-		body = ""
-		topic_name = ""
-		for li in contents:
-			if type(li).__name__ == 'dict':
-				if 'type' in li and li['type'] == 'kicker':
-					# skip filter kickers
-					topic_name = li['content']
-					if topic_name in filter_kicker.keys():
-						skip = True
-						break
-				if 'subtype' in li and li['subtype'] == 'paragraph':
-					paragraph = li['content'].strip()
-					# Replace <.*?> with ""
-					paragraph = re.sub(r'<.*?>', '', paragraph)
-					body += ' ' + paragraph
-		if skip:
-			kicker_filterd_mp[cnt] = 1
-			continue
-		# Recall By tf_idf
-		body = body.strip()
-		res_tfidf = tfidf.recall_by_tfidf_fast([body, '20', nlp, words_mp, words_idx])
+	with open(cfg.OUTPUT + 'Dataset_BertCls.txt', 'w', encoding='utf-8') as out:
+		for cnt in tqdm(range(1, len(WashingtonPost))):
+			kicker_filterd_mp = {}		# Record line filtered by kicker, line index start from 1
+			line = WashingtonPost[cnt]
+			obj = json.loads(line)
+			contents = obj['contents']
+			title = obj['title']
+			author = obj['author']
+			date = obj['published_date']
+			skip = False
+			body = ""
+			topic_name = ""
+			for li in contents:
+				if type(li).__name__ == 'dict':
+					if 'type' in li and li['type'] == 'kicker':
+						# skip filter kickers
+						topic_name = li['content']
+						if topic_name in filter_kicker.keys():
+							skip = True
+							break
+					if 'subtype' in li and li['subtype'] == 'paragraph':
+						paragraph = li['content'].strip()
+						# Replace <.*?> with ""
+						paragraph = re.sub(r'<.*?>', '', paragraph)
+						body += ' ' + paragraph
+			if skip:
+				kicker_filterd_mp[cnt] = 1
+				continue
+			# Recall By tf_idf
+			body = body.strip()
+			res_tfidf = tfidf.recall_by_tfidf_fast([body, '20', nlp, words_mp, words_idx])
 
-		# Recall By topics
-		res_topic = topic.recall_by_topics([topic_name, topics_idx])
+			# Recall By topics
+			res_topic = topic.recall_by_topics([topic_name, topics_idx])
 
-		# Combie Recall results
-		res_mask = {}
-		for li in res_tfidf:
-			# Filter by kicker
-			if int(li) in kicker_filterd_mp:
-				continue
-			res_mask[int(li)] = 4
-		for li in res_topic:
-			# Filter by kicker
-			if int(li) in kicker_filterd_mp:
-				continue
-			if li in res_mask:
-				res_mask[int(li)] = 8
-			else:
-				res_mask[int(li)] = 2
+			# Combie Recall results
+			res_mask = {}
+			for li in res_tfidf:
+				# Filter by kicker
+				if int(li) in kicker_filterd_mp:
+					continue
+				res_mask[int(li)] = 4
+			for li in res_topic:
+				# Filter by kicker
+				if int(li) in kicker_filterd_mp:
+					continue
+				if li in res_mask:
+					res_mask[int(li)] = 8
+				else:
+					res_mask[int(li)] = 2
 
-		# Filter
-		key_list = sorted(res_mask)
-		similar_doc = {}
-		similar_doc[title + '#' + author + '#' + str(date)] = 1
-		doc_cache = {}		# cache document, classify by 0, 2, 4, 8
-		doc_cache[0] = []
-		for li_cnt in key_list:
-			li = WashingtonPost[li_cnt]
-			doc = json.loads(li)
-			doc_title = doc['title']
-			doc_author = doc['author']
-			doc_date = doc['published_date']
-			doc_contents = doc['contents']
-			# Filter by date
-			if doc_date is not None and date is not None and int(doc_date) > int(date):
-				continue
-			# Filter by date + title + author
-			rep_key = doc_title + '#' + doc_author + '#' + str(doc_date)
-			if rep_key in similar_doc:
-				continue
-			else:
-				similar_doc[rep_key] = 1
-			# cache document
-			if res_mask[li_cnt] not in doc_cache:
-				doc_cache[res_mask[li_cnt]] = []
-			doc_cache[res_mask[li_cnt]].append(doc)
+			# Filter
+			key_list = sorted(res_mask)
+			similar_doc = {}
+			similar_doc[title + '#' + author + '#' + str(date)] = 1
+			doc_cache = {}		# cache document, classify by 0, 2, 4, 8
+			doc_cache[0] = []
+			for li_cnt in key_list:
+				li = WashingtonPost[li_cnt]
+				doc = json.loads(li)
+				doc_title = doc['title']
+				doc_author = doc['author']
+				doc_date = doc['published_date']
+				doc_contents = doc['contents']
+				# Filter by date
+				if doc_date is not None and date is not None and int(doc_date) > int(date):
+					continue
+				# Filter by date + title + author
+				rep_key = doc_title + '#' + doc_author + '#' + str(doc_date)
+				if rep_key in similar_doc:
+					continue
+				else:
+					similar_doc[rep_key] = 1
+				# cache document
+				if res_mask[li_cnt] not in doc_cache:
+					doc_cache[res_mask[li_cnt]] = []
+				doc_cache[res_mask[li_cnt]].append(doc)
 
-		# random add 100 label 0 document
-		zero = np.random.randint(1, len(WashingtonPost), size=[100])
-		for li_cnt in zero:
-			doc = json.loads(WashingtonPost[li_cnt])
-			doc_date = doc['published_date']
-			# Filter by kicker
-			if li_cnt in kicker_filterd_mp:
-				continue
-			# Filter by date
-			if doc_date is not None and date is not None and int(doc_date) > int(date):
-				continue
-			# Filter by date + title + author
-			rep_key = doc_title + '#' + doc_author + '#' + str(doc_date)
-			if rep_key in similar_doc:
-				continue
-			else:
-				similar_doc[rep_key] = 1
-			doc_cache[0].append(doc)
+			# random add 100 label 0 document
+			zero = np.random.randint(1, len(WashingtonPost), size=[100])
+			for li_cnt in zero:
+				doc = json.loads(WashingtonPost[li_cnt])
+				doc_date = doc['published_date']
+				# Filter by kicker
+				if li_cnt in kicker_filterd_mp:
+					continue
+				# Filter by date
+				if doc_date is not None and date is not None and int(doc_date) > int(date):
+					continue
+				# Filter by date + title + author
+				rep_key = doc_title + '#' + doc_author + '#' + str(doc_date)
+				if rep_key in similar_doc:
+					continue
+				else:
+					similar_doc[rep_key] = 1
+				doc_cache[0].append(doc)
 
-		# split from body
-		sen1 = split_body([body, max_length, nlp])
-		# Sampling and Generate examples
-		with open(cfg.OUTPUT + 'Dataset_BertCls.txt', 'w', encoding='utf-8') as out:
+			# split from body
+			sen1 = split_body([body, max_length, nlp])
+
+			# Sampling and Generate examples
 			# label 0, 2, 4, 8
 			for label in sorted(doc_cache):
 				if len(doc_cache[label]) <= 0:
