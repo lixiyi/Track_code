b9f865683220320a98a1f72dc300e3c4741d17cc
lixiyi <609991496@qq.com>
2019-07-29 18:11:04 +0800

    modify

--- src/Recall/TFIDF_spark.py
+++ src/Recall/TFIDF_spark.py
@@ -72,52 +72,54 @@ def tfidf_index(args = None):
 	sc = SparkContext(conf=conf)
 	filter_kicker = {"Opinion": 1, "Letters to the Editor": 1, "The Post's View": 1}
 	WashingtonPost = sc.textFile(path_mp['DataPath'] + path_mp['WashingtonPost'])
-	# read words_mp and words_idx into memory first(idx start from 1)
-	def tfidf_index_single(line, filter_kicker, num):
-		obj = json.loads(line)
-		doc_id = obj['id']
-		contents = obj['contents']
-		doc = ""
-		for li in contents:
-			if type(li).__name__ == 'dict':
-				if 'type' in li and li['type'] == 'kicker':
-					# skip filter kickers
-					if li['content'] in filter_kicker.keys():
-						return ()
-				if 'subtype' in li and li['subtype'] == 'paragraph':
-					paragraph = li['content'].strip()
-					# Replace <.*?> with ""
-					paragraph = re.sub(r'<.*?>', '', paragraph)
-					doc += ' ' + paragraph
-		doc = doc.strip()
-		w_list = cfg.word_cut(doc)
-		num = int(num)
-		# calculate term frequency for each word in the str
-		tf = {}
-		for w in w_list:
-			if w in tf:
-				tf[w] += 1
-			else:
-				tf[w] = 1
-		# calculate idf and tf-idf for each word
-		tfidf_val = {}
-		for w in w_list:
-			# word not in vocabulary
-			if w not in words_mp:
-				continue
-			idf = np.log(cfg.DOCUMENT_COUNT * 1.0 / len(words_mp[w]))
-			tfidf_val[w] = tf[w] * 1.0 * idf
-		# sort by tf-idf, combine top inverted file line number list
-		tfidf_val = sorted(tfidf_val.items(), key=lambda d: d[1], reverse=True)
-		res = set()
-		for i in range(min(num, len(tfidf_val))):
-			w = tfidf_val[i][0]
-			res = res | set(words_mp[w])
-		return doc_id + ' ' + ' '.join(res)
-	WashingtonPost.map(lambda line: tfidf_index_single(line, filter_kicker, 20)) \
+	WashingtonPost.map(lambda line: tfidf_index_single(line, filter_kicker, words_mp, 20)) \
 		.filter(lambda w: w != ()) \
 		.saveAsTextFile(cfg.OUTPUT + 'tfidf_index')
 	sc.stop()
+
+
+# read words_mp and words_idx into memory first(idx start from 1)
+def tfidf_index_single(line, filter_kicker, words_mp, num):
+	obj = json.loads(line)
+	doc_id = obj['id']
+	contents = obj['contents']
+	doc = ""
+	for li in contents:
+		if type(li).__name__ == 'dict':
+			if 'type' in li and li['type'] == 'kicker':
+				# skip filter kickers
+				if li['content'] in filter_kicker.keys():
+					return ()
+			if 'subtype' in li and li['subtype'] == 'paragraph':
+				paragraph = li['content'].strip()
+				# Replace <.*?> with ""
+				paragraph = re.sub(r'<.*?>', '', paragraph)
+				doc += ' ' + paragraph
+	doc = doc.strip()
+	w_list = cfg.word_cut(doc)
+	num = int(num)
+	# calculate term frequency for each word in the str
+	tf = {}
+	for w in w_list:
+		if w in tf:
+			tf[w] += 1
+		else:
+			tf[w] = 1
+	# calculate idf and tf-idf for each word
+	tfidf_val = {}
+	for w in w_list:
+		# word not in vocabulary
+		if w not in words_mp:
+			continue
+		idf = np.log(cfg.DOCUMENT_COUNT * 1.0 / len(words_mp[w]))
+		tfidf_val[w] = tf[w] * 1.0 * idf
+	# sort by tf-idf, combine top inverted file line number list
+	tfidf_val = sorted(tfidf_val.items(), key=lambda d: d[1], reverse=True)
+	res = set()
+	for i in range(min(num, len(tfidf_val))):
+		w = tfidf_val[i][0]
+		res = res | set(words_mp[w])
+	return doc_id + ' ' + ' '.join(res)
 
 
 if __name__ == "__main__":
