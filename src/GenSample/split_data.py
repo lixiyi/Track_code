f562d3a04565f5a713b0bf39e95642ed6c11b563
lixiyi <609991496@qq.com>
2019-07-30 17:19:10 +0800

    split_data

--- src/GenSample/split_data.py
+++ src/GenSample/split_data.py
@@ -15,7 +15,7 @@ path_mp = cfg.get_path_conf('../path.cfg')
 with open(cfg.OUTPUT + 'train.txt', 'w', encoding='utf-8') as out1:
 	with open(cfg.OUTPUT + 'dev.txt', 'w', encoding='utf-8') as out2:
 		with open(cfg.OUTPUT + 'test.txt', 'w', encoding='utf-8') as out3:
-			with open(cfg.OUTPUT + 'tmp.txt', 'w', encoding='utf-8') as f:
+			with open(cfg.OUTPUT + 'tmp.txt', 'r', encoding='utf-8') as f:
 				cnt = 0
 				for line in tqdm(f):
 					if cnt < 100:
