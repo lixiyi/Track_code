9e7532d05930c454d5c258a5b7ba6c0013882688
lixiyi <609991496@qq.com>
2019-07-30 17:17:48 +0800

    modify

--- src/GenSample/GenBertCls_fast.py
+++ src/GenSample/GenBertCls_fast.py
@@ -100,100 +100,94 @@ def gen_sample(args=None):
 			words_index[li[0]] = set(li[1:])
 	print('words idx loaded.')
 
-	examples = []
-
-	for cur_id in tqdm(tfidf_list):
-		obj = WashingtonPost[cur_id]
-		contents = obj['contents']
-		title = obj['title']
-		author = obj['author']
-		date = obj['published_date']
-		body = ""
-		topic_name = ""
-		for li in contents:
-			if type(li).__name__ == 'dict':
-				if 'type' in li and li['type'] == 'kicker' and topic_name != "":
-					topic_name = li['content'].strip()
-				if 'subtype' in li and li['subtype'] == 'paragraph':
-					paragraph = li['content'].strip()
-					# Replace <.*?> with ""
-					paragraph = re.sub(r'<.*?>', '', paragraph)
-					body += ' ' + paragraph
-		# Recall By tf_idf
-		body = body.strip()
-		res_tfidf = set()
-		for w in tfidf_mp[cur_id]:
-			res_tfidf = res_tfidf | words_index[w]
-		res_tfidf = list(res_tfidf)
-
-		# Recall By topics
-		res_topic = []
-		if topic_name in res_topic:
-			res_topic = list(topics_mp[topic_name])
-
-		# Combie Recall results
-		similar_doc = {} # Filter
-		cur_key = ''
-		if title is not None:
-			cur_key += title
-		if author is not None:
-			cur_key += '#' + author
-		if date is None:
-			cur_key += '#' + str(date)
-		similar_doc[cur_key] = 1
-		res_mask = {}
-		res_mask[0] = set()
-		res_mask[1] = set()
-		res_mask[2] = set()
-		res_mask[3] = set()
-		res_tfidf_mp = {} 	# help decide which is 8
-		for li in res_tfidf:
-			# Filter by kicker
-			if li in tfidf_mp and filter_doc(WashingtonPost[li], date, similar_doc):
-				res_mask[2].add(li)
-				res_tfidf_mp[li] = 1
-		for li in res_topic:
-			# Filter by kicker
-			if li in tfidf_mp and filter_doc(WashingtonPost[li], date, similar_doc):
-				if li in res_tfidf_mp:
-					res_mask[3].add(li)
-				else:
-					res_mask[1].add(li)
-
-		# random add 100 label 0 document
-		zero = np.random.randint(0, len(tfidf_mp), size=[100])
-		for li in zero:
-			doc_id = tfidf_list[li]
-			if filter_doc(WashingtonPost[doc_id], date, similar_doc):
-				res_mask[0].add(doc_id)
-
-		# split from body
-		sen1 = split_body([body, max_length])
-
-		# Sampling and Generate examples
-		# label 0, 2, 4, 8
-		for label in res_mask.keys():
-			res_mask[label] = list(res_mask[label])
-			if len(res_mask[label]) <= 0:
-				continue
-			idx = random.randint(0, len(res_mask[label])-1)
-			doc_id = res_mask[label][idx]
-			doc_body = extract_body([WashingtonPost[doc_id]['contents']])
-			sen2 = split_body([doc_body, max_length])
-			# out.write(str(label) + '\t' + sen1 + '\t' + sen2 + '\n')
-			examples.append(str(label) + '\t' + sen1 + '\t' + sen2 + '\n')
-
-		# label 16 middle from the body
-		w_list = cfg.word_cut(body)
-		st = (len(w_list) - max_length + 2) //2
-		ed = st + max_length - 2
-		sen2 = ' '.join(w_list[st:ed])
-		# out.write(str(4) + '\t' + sen1 + '\t' + sen2 + '\n')
-		examples.append(str(4) + '\t' + sen1 + '\t' + sen2 + '\n')
-
 	with open(cfg.OUTPUT + 'Dataset_BertCls.txt', 'w', encoding='utf-8') as out:
-		for li in tqdm(examples):
-			out.write(li)
+		for cur_id in tqdm(tfidf_list):
+			obj = WashingtonPost[cur_id]
+			contents = obj['contents']
+			title = obj['title']
+			author = obj['author']
+			date = obj['published_date']
+			body = ""
+			topic_name = ""
+			for li in contents:
+				if type(li).__name__ == 'dict':
+					if 'type' in li and li['type'] == 'kicker' and topic_name != "":
+						topic_name = li['content'].strip()
+					if 'subtype' in li and li['subtype'] == 'paragraph':
+						paragraph = li['content'].strip()
+						# Replace <.*?> with ""
+						paragraph = re.sub(r'<.*?>', '', paragraph)
+						body += ' ' + paragraph
+			# Recall By tf_idf
+			body = body.strip()
+			res_tfidf = set()
+			for w in tfidf_mp[cur_id]:
+				res_tfidf = res_tfidf | words_index[w]
+			res_tfidf = list(res_tfidf)
+
+			# Recall By topics
+			res_topic = []
+			if topic_name in res_topic:
+				res_topic = list(topics_mp[topic_name])
+
+			# Combie Recall results
+			similar_doc = {} # Filter
+			cur_key = ''
+			if title is not None:
+				cur_key += title
+			if author is not None:
+				cur_key += '#' + author
+			if date is None:
+				cur_key += '#' + str(date)
+			similar_doc[cur_key] = 1
+			res_mask = {}
+			res_mask[0] = set()
+			res_mask[1] = set()
+			res_mask[2] = set()
+			res_mask[3] = set()
+			res_tfidf_mp = {} 	# help decide which is 8
+			for li in res_tfidf:
+				# Filter by kicker
+				if li in tfidf_mp and filter_doc(WashingtonPost[li], date, similar_doc):
+					res_mask[2].add(li)
+					res_tfidf_mp[li] = 1
+			for li in res_topic:
+				# Filter by kicker
+				if li in tfidf_mp and filter_doc(WashingtonPost[li], date, similar_doc):
+					if li in res_tfidf_mp:
+						res_mask[3].add(li)
+					else:
+						res_mask[1].add(li)
+
+			# random add 100 label 0 document
+			zero = np.random.randint(0, len(tfidf_mp), size=[100])
+			for li in zero:
+				doc_id = tfidf_list[li]
+				if filter_doc(WashingtonPost[doc_id], date, similar_doc):
+					res_mask[0].add(doc_id)
+
+			# split from body
+			sen1 = split_body([body, max_length])
+
+			# Sampling and Generate examples
+			# label 0, 2, 4, 8
+			for label in res_mask.keys():
+				res_mask[label] = list(res_mask[label])
+				if len(res_mask[label]) <= 0:
+					continue
+				idx = random.randint(0, len(res_mask[label])-1)
+				doc_id = res_mask[label][idx]
+				doc_body = extract_body([WashingtonPost[doc_id]['contents']])
+				sen2 = split_body([doc_body, max_length])
+				out.write(str(label) + '\t' + sen1 + '\t' + sen2 + '\n')
+
+			# label 16 middle from the body
+			w_list = cfg.word_cut(body)
+			st = (len(w_list) - max_length + 2) //2
+			ed = st + max_length - 2
+			sen2 = ' '.join(w_list[st:ed])
+			out.write(str(4) + '\t' + sen1 + '\t' + sen2 + '\n')
+
 
 if __name__ == "__main__":
 	getattr(__import__('GenBertCls_fast'), sys.argv[1])(sys.argv[2:])
---
+++ src/GenSample/split_data.py
@@ -0,0 +1,27 @@
+import sys
+sys.path.append("..")
+
+
+import DataProcess.getCfg as cfg
+import json
+import re
+from tqdm import tqdm
+import random
+import numpy as np
+
+
+path_mp = cfg.get_path_conf('../path.cfg')
+
+with open(cfg.OUTPUT + 'train.txt', 'w', encoding='utf-8') as out1:
+	with open(cfg.OUTPUT + 'dev.txt', 'w', encoding='utf-8') as out2:
+		with open(cfg.OUTPUT + 'test.txt', 'w', encoding='utf-8') as out3:
+			with open(cfg.OUTPUT + 'tmp.txt', 'w', encoding='utf-8') as f:
+				cnt = 0
+				for line in tqdm(f):
+					if cnt < 100:
+						out3.write(line)
+					elif cnt < 200:
+						out2.write(line)
+					else:
+						out1.write(line)
+					cnt += 1
