d066c1eacffc772dd7daa099fc07d9c1cb189133
lixiyi <609991496@qq.com>
2019-07-30 17:30:04 +0800

    copy.sh

--- src/Ranking/copy.sh
+++ src/Ranking/copy.sh
@@ -2,15 +2,15 @@
 
 cp /home/trec7/lianxiaoying/Track_code/src/Ranking/run_classifier.py /home/trec7/lianxiaoying/bert
 
-BERT_BASE_DIR=/home/trec7/lianxiaoying/model/uncased_L-12_H-768_A-12
-DATA_DIR=/home/trec7/lianxiaoying/Track_code/src/outputs
+export BERT_BASE_DIR=/home/trec7/lianxiaoying/model/uncased_L-12_H-768_A-12
+export DATA_DIR=/home/trec7/lianxiaoying/Track_code/src/outputs
 
 python /home/trec7/lianxiaoying/bert/run_classifier.py \
   --task_name=bertcls \
   --do_train=true \
   --do_eval=true \
   --do_test=true \
-  --data_dir=$DATA_DIR_DIR \
+  --data_dir=$DATA_DIR \
   --vocab_file=$BERT_BASE_DIR/vocab.txt \
   --bert_config_file=$BERT_BASE_DIR/bert_config.json \
   --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
