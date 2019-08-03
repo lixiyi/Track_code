d031876c84671f6db56d6dab38e92c95cc9d31b2
lixiyi <609991496@qq.com>
2019-08-03 16:29:58 +0800

    elastic

--- src/elastic/Query_2.py
+++ src/elastic/Query_2.py
@@ -57,7 +57,7 @@ def test_backgound_linking():
 			# 		qr.append(w)
 			# qr = ' '.join(qr)
 			# query the doc
-			qr = doc['title_body'][:512]
+			qr = doc['title_body'][:512] + doc['title_body'][-512:]
 			dsl = {
 				"size": 1000,
 				"timeout": "1m",
