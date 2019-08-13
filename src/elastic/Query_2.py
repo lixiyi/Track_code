7218d72693f10cb52bccb6af84d4af2afdaa03b0
lixiyi <609991496@qq.com>
2019-08-13 10:09:47 +0800

    elastic

--- src/elastic/Query_2.py
+++ src/elastic/Query_2.py
@@ -113,7 +113,7 @@ def test_backgound_linking():
 							'match': {
 								'title_body': {
 									'query': qr,
-									'boost': 3
+									'boost': 2
 								}
 							}
 						},
@@ -122,7 +122,7 @@ def test_backgound_linking():
 								'match': {
 									'title_body': {
 										'query': doc['title'],
-										"boost": 5
+										"boost": 6
 									}
 								}
 							},
