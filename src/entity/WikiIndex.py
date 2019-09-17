6c7c8e100f8a6a6f34e571b63cd9e20e601d5315
lixiyi <609991496@qq.com>
2019-09-17 14:05:32 +0800

    entity bm25

--- src/entity/WikiIndex.py
+++ src/entity/WikiIndex.py
@@ -12,15 +12,15 @@ from elasticsearch import Elasticsearch
 # get file path conf
 path_mp = cfg.get_path_conf('../path.cfg')
 es = Elasticsearch(port=7200)
-INDEX_NAME = "news_wiki"
+INDEX_NAME = "news_wiki_entity_stem"
 
 setting = {
 	"index": {
 		"similarity": {
 			"my_bm25": {
 				"type": "BM25",
-				"b": 0.75,
-				"k1": 1.2
+				"b": 0.95,
+				"k1": 2.0
 			}
 		}
 	}
